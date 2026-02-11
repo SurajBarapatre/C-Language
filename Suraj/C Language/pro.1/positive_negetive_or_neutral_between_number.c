00361fa9e0f1ee2b59f8589ebe11e3728178bd0f
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-02-11 20:26:05 +0530

    Add files via upload

---
+++ Suraj/C Language/pro.1/positive_negetive_or_neutral_between_number.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+void main()
+{
+	//wap to check number is positive,negetive or neutral?
+int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if (number > 0) {
+        printf("%d is a positive number.\n", number);
+    } 
+    else if (number < 0) {
+        printf("%d is a negative number.\n", number);
+    } 
+    else {
+        printf("The number is zero.\n");
+    }
+}
