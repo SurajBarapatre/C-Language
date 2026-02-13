57a2d0cbfa633170003d5b199591e13ddd494fbe
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-02-13 10:29:55 +0530

    Update positive_negetive_or_neutral_between_number.c

--- Suraj/C Language/pro.1/positive_negetive_or_neutral_between_number.c
+++ Suraj/C Language/pro.1/positive_negetive_or_neutral_between_number.c
@@ -11,10 +11,12 @@ int number;
     if (number > 0) {
         printf("%d is a positive number.\n", number);
     } 
-    else if (number < 0) {
+    else 
+		if (number < 0) {
         printf("%d is a negative number.\n", number);
     } 
     else {
         printf("The number is zero.\n");
     }
 }
+
