6c3c0b161c3adbdceff8c719605c83a03dc8657e
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-02-19 23:12:59 +0530

    Add files via upload

---
+++ Suraj/practical.6/pro.1.c
@@ -0,0 +1,28 @@
+
+#include<stdio.h>
+
+void main ()
+
+{
+	//wap to check is negetive,positive or neutral by using ladder if
+  int num;
+  printf("enter number :");
+  scanf ("%d",&num);
+if(num==0)
+  {
+  	printf("Number is Neutral.");
+  }
+else if(num>=0)
+ {
+ 	printf("Number is Positive !");
+ }
+else if(num<=0)
+ {
+ 	printf("Number is Negetive !");
+ }
+ 
+	 	
+	
+	
+	
+}
---
+++ Suraj/practical.6/pro.2.c
@@ -0,0 +1,29 @@
+
+#include<stdio.h>
+
+void main ()
+{
+	//wap to find maximum from 2 variables using else if ladder
+	
+	int x,y;
+	printf("Enter x :");
+	scanf ("%d",&x);
+	printf("Enter y :");
+	scanf("%d",&y);
+if(x>=y)
+{
+printf("x is maximum !");
+}
+	
+else
+{
+printf("y is maximum !");
+}	
+	
+	
+	
+	
+	
+	
+	
+}
