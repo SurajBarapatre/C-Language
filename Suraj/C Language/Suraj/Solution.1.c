c1cee5d821e200b47554e7cb3c3c9dcaf103b966
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-02-23 22:56:01 +0530

    Update Solution.1.c

--- Suraj/C Language/Suraj/Solution.1.c
+++ Suraj/C Language/Suraj/Solution.1.c
@@ -2,15 +2,16 @@
 
 void main()
 {
-	// devlop a program that prints all alphabets from 'a' to 'z' by skipping 3 alphabets using a for loop
+	// devlop a program that prints all alphabets from 'a' to 'z' by skipping 3 alphabets using a do while loop
 	
 	 char i;
-	 
-	for (i='a';i<='z';i+=4)
+	 i='a';
+	do 
 	{
-		printf("%c",i);
-		
-	}
+		printf("%c\n",i);
+	    i+=4;
+	} while (i<='z');
 	
 }
+
 
