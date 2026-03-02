a4cff0a3b742b33ac3559c57194253ce0ba6de4f
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-03-02 10:44:44 +0530

    Update task.2.c

--- Suraj/C Language/Pattern Loop/pattern 2/task.2.c
+++ Suraj/C Language/Pattern Loop/pattern 2/task.2.c
@@ -15,16 +15,5 @@ void main()
 	}
 }
 
-//a=1, 1<=5:true,     
-
-//a=1, 1<=5:true, b=1, 1<=1:true,
-
-//a=2, 2<=5:true, b=2, 2<=2:true,
-
-//a=3, 3<=5:true, b=3, 3<=3:true,
-
-//a=4, 4<=5:true, b=4, 4<=4:true,
-
-//a=5, 5<=5:true, b=5, 5<=5:true,
 
 
