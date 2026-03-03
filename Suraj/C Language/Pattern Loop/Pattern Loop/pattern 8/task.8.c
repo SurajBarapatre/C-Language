1ea514cee8fede6c5a8e288d592e0ede5e4fe4a8
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-03-03 20:59:33 +0530

    Update task.8.c

--- Suraj/C Language/Pattern Loop/Pattern Loop/pattern 8/task.8.c
+++ Suraj/C Language/Pattern Loop/Pattern Loop/pattern 8/task.8.c
@@ -4,13 +4,14 @@ void main()
 {
 	int a,b;
 	
-	for(a=1; a<=5; a++)
+	for(a=1; a<=5; a++) // outer loop for each starting number
 	{
-		for(b=a; b<=5; b++)
+		for(b=a; b<=5; b++) // inner loop for printing number starting for 'a'.
 		{
 			printf("%d ",b);
 		}
-		printf("\n");
+		printf("\n"); // new line after new row
 	}
 }
+
 
