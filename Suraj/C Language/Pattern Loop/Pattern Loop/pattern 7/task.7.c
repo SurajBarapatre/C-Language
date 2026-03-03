5f7ed62a5f760b7caf0235129dda5ab16033662f
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-03-03 20:56:38 +0530

    Update task.7.c

--- Suraj/C Language/Pattern Loop/Pattern Loop/pattern 7/task.7.c
+++ Suraj/C Language/Pattern Loop/Pattern Loop/pattern 7/task.7.c
@@ -4,13 +4,14 @@ void main()
 {
 	int i,j;
 	
-	for(i=5; i>=1; i--)
+	for(i=5; i>=1; i--) // outer loop for each row
 	{
-		for(j=1; j<=i; j++)
+		for(j=1; j<=i; j++) // inner loop for printing numbers
 		{
 			printf("%d ",j);
 		}
-		printf("\n");
+		printf("\n"); // new line after new row
 	}
 }
+
 
