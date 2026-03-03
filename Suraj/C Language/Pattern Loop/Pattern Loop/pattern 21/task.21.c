d5c5a84a76d0f05ba3353fbfdba2d8eb5837a717
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-03-03 21:34:43 +0530

    Update task.21.c

--- Suraj/C Language/Pattern Loop/Pattern Loop/pattern 21/task.21.c
+++ Suraj/C Language/Pattern Loop/Pattern Loop/pattern 21/task.21.c
@@ -5,15 +5,16 @@ void main()
 {
 	char i,j;
 	
-	for(i='A'; i<='E'; i++)
+	for(i='A'; i<='E'; i++) // Outer loop for each row
 	{
-		for(j='A'; j<=i; j++)
+		for(j='A'; j<=i; j++) // Inner loop for printing letters from A to the i-th letter
 		{
-			printf("%c ",j);		
+			printf("%c ",j); // Print the current character followed by a space	
 		}
-		printf("\n");
+		printf("\n"); // new line after new row
 	}
 }
+
 
 
 
