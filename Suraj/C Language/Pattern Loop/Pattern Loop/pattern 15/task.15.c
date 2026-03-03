db1658586cadcc7bebc272ee918081d174818114
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-03-03 21:15:39 +0530

    Update task.15.c

--- Suraj/C Language/Pattern Loop/Pattern Loop/pattern 15/task.15.c
+++ Suraj/C Language/Pattern Loop/Pattern Loop/pattern 15/task.15.c
@@ -4,11 +4,11 @@ void main()
 {
 	int i,j;
 	
-	for(i=1; i<=5; i++)
+	for(i=1; i<=5; i++) // Outer loop for each row
 	{
-		for(j=1; j<=i; j++)
+		for(j=1; j<=i; j++) // Inner loop for printing 1 and 0
 		
-		if ((i+j)%2==0)
+		if ((i+j)%2==0) // Print 1 if the sum of row index and column index is even, otherwise print 0
 		{
 		printf("1 ");
 		}
@@ -19,4 +19,5 @@ void main()
 		printf("\n");
 	}
 }
+
 
