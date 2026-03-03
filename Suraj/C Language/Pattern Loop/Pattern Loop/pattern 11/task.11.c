258ca358336d26304aba082124aa9ab55d9543a5
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-03-03 21:06:20 +0530

    Update task.11.c

--- Suraj/C Language/Pattern Loop/Pattern Loop/pattern 11/task.11.c
+++ Suraj/C Language/Pattern Loop/Pattern Loop/pattern 11/task.11.c
@@ -4,14 +4,15 @@ void main()
 {
 	int i,j;
 	
-	for(i=5; i>=1; i--)
+	for(i=5; i>=1; i--) // Outer loop for each row
 	{
-		for(j=i; j>=1; j--)
+		for(j=i; j>=1; j--) // Inner loop for printing numbers from (5-i) down to 1
 		{
 			printf("%d ",j);
 		}
-		printf("\n");
+		printf("\n"); // new line after new row
 	}
 }
+
 
 
