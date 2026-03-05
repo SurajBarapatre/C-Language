bf8997cb520c0a32b2b0d6b3afdd90ee41e020e0
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-03-05 08:59:59 +0530

    Update task.6.c

--- Suraj/C Language/pattern loop with space/pattern 6/task.6.c
+++ Suraj/C Language/pattern loop with space/pattern 6/task.6.c
@@ -4,9 +4,9 @@ void main()
 {
 	int i,j,k=5;
 	
-	for(i=k; i>=5; i--) // Outer loop for rows
+	for(i=k; i>=1; i--) // Outer loop for rows
 	{
-		for(j=1; j<=5-i; j++) // Inner loop for printing spaces
+		for(j=1; j<=k-i; j++) // Inner loop for printing spaces
 		{
 			printf("  "); // Printing two spaces for alignment
 		}
@@ -17,5 +17,4 @@ void main()
 		printf("\n"); // new line after new row
 	}
 }
-
 
