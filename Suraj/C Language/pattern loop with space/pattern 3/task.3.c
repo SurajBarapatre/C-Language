2ddf7cb0819d1b11ba09004f5d29fcd95bed9b32
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-03-06 19:58:18 +0530

    Update task.3.c

--- Suraj/C Language/pattern loop with space/pattern 3/task.3.c
+++ Suraj/C Language/pattern loop with space/pattern 3/task.3.c
@@ -2,6 +2,13 @@
 
 void main()
 {
+	     5
+       4 5
+     3 4 5
+   2 3 4 5
+ 1 2 3 4 5
+	
+	
 	int i,j,k;
 	
 	for(i=5; i>=1; i--) // Outer loop for rows
@@ -17,3 +24,4 @@ void main()
 		printf("\n"); // new line after new row
 	}
 }
+
