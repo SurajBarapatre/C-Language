40e822d697f1aa7c4e77aac5306ac7f956011aa0
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-03-06 10:17:31 +0530

    Update task.3.c

--- Suraj/C Language/Pattern Loop/Pattern Loop/pattern 3/task.3.c
+++ Suraj/C Language/Pattern Loop/Pattern Loop/pattern 3/task.3.c
@@ -3,6 +3,12 @@
 
 void main()
 {
+  //  1
+  //  2 2
+//    3 3 3
+//    4 4 4 4
+//    5 5 5 5 5
+
 	int i,j; // number of lines for the pattern
 	
 	for(i=1; i<=5; i++) // outer loop for number of rows
@@ -14,4 +20,5 @@ void main()
 		printf("\n"); // new line after new row
 	}
 }
+
 
