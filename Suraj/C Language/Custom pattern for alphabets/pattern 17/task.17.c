effbbd4808a0cd5382c71f1262dbe601302b6285
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-03-11 10:40:50 +0530

    Update task.17.c

--- Suraj/C Language/Custom pattern for alphabets/pattern 17/task.17.c
+++ Suraj/C Language/Custom pattern for alphabets/pattern 17/task.17.c
@@ -11,28 +11,41 @@ void main()
 //   *  *
 //         * 	
 	
-	int i,j;
-
-    for(i=1; i<=7; i++)
+int i,j;  
+    
+    for(i=1; i<=8; i++)		
     {
-        for(j=1; j<=6; j++)
-        {
-            if((i==1 && j<=5) ||           // for top row
-               (i==7 && j<=5) ||           // for bottom row
-               (j==1 && i>=2 && i<=6) ||   // for left side
-               (j==6 && i>=2 && i<=4) ||   // for right side
-               (i==4 && j==4) ||           // for inner star
-               (i==6 && j==6))             // for tail of Q
-            {
-                printf("* ");
-            }
-            else
-            {
-                printf("  ");
-            }
-        }
-        printf("\n");
-    }
-
+    	for(j=1; j<=6; j++)		
+    	{
+    		if((i==1 || i==7) && (j>=1 && j<=5))	// for top row
+    		{
+    			printf("* ");
+			}
+			else if(j==1 && (i>=1 && i<=7)) // for bottom row
+			{
+				printf("* ");
+			}
+			else if(j==5 && (i>=1 && i<=6)) // for left side
+			{
+				printf("* ");
+			}
+			else if(i==6 && j==5 ) // for right side
+			{
+				printf("* ");
+			}
+			else if(i==5 && j==4 )  // for inner star
+			{
+				printf("* ");
+			}
+			else if(i==7 && j==6)  // for tail of Q
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
 }
-
