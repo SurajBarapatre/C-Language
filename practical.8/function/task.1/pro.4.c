78be9afa4b8812e200a79f24a0ac51659ad8fb96
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-03-26 21:18:18 +0530

    Update pro.4.c

--- practical.8/function/task.1/pro.4.c
+++ practical.8/function/task.1/pro.4.c
@@ -2,22 +2,22 @@
 
 // print to area of circle by using function
 
-float findArea(float r)
+float findArea(float r)                         // for finding area of circle 
 {
     float area;
-    area = 3.14 * r * r;
-    return area;
+    area = 3.14 * r * r;                        // for using formula to find area of circle
+    return area;                               //  for return the calculated area 
 }
 
-void main()
+void main()                                    // for user input radius
 {
     float radius, area_of_circle;
 
-    printf("Enter radius of circle: ");
+    printf("Enter radius of circle: ");        // for printing radius
     scanf("%f", &radius);
 
-    area_of_circle = findArea(radius);
+    area_of_circle = findArea(radius);           // for funtion when we call it
 
-    printf("Area of Circle = %.2f",area_of_circle);
+    printf("Area of Circle = %.2f",area_of_circle);     // for display area of circle
 
 }
