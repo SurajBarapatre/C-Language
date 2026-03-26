fcda12e483a0de285b4fd281195440341ccf15f2
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-03-26 10:28:45 +0530

    Update pro.2.c

--- practical.8/function/task.1/pro.2.c
+++ practical.8/function/task.1/pro.2.c
@@ -1,23 +1,3 @@
-#include <stdio.h>
+#include<stdio.h>
 
-// Function to calculate sum
-int sum(int a, int b)
-{
-    return a + b;
-}
-
-int main()
-{
-    int num1, num2, result;
-
-    // Input numbers
-    printf("Enter two numbers: ");
-    scanf("%d %d", &num1, &num2);
-
-    // Function call
-    result = sum(num1, num2);
-
-    // Output result
-    printf("Sum = %d", result);
-
-}
+// print to sum of two num
