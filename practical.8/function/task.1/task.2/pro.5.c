473f0376e878c8252b20b39fbab7f131f61fbffb
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-03-31 10:19:57 +0530

    Add files via upload

---
+++ practical.8/function/task.1/task.2/pro.5.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void main()
+
+{
+	// wap to print only even elements of array
+	
+	int n,i;
+	
+	printf("Enter the Size of array : "); // for input size of array
+	scanf("%d",&n);
+	
+	int a[n];                 
+	
+	for(i=1; i<=n; i++)
+	{
+		printf("Enter a %d: ",i); // for input elements
+		scanf("%d",&a[i]);
+	}
+	
+	printf("Even Elements in the array :\n"); // for print even elements
+	for(i=1; i<=n; i++)
+	{
+		if(a[i]%2==0)
+		{
+			printf("%d ",a[i]);
+		}
+	}
+}
+
+
