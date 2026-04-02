bfc800526c90404fde69427a04c5b5ffa0c3a30c
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-04-02 10:47:40 +0530

    Add files via upload

---
+++ Suraj/PR.8/pro.1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+// develop a program that finds the length of a string using a pointer
+
+int stringLength(char *str)
+{
+
+int length =0;
+
+while(*str!='\0')
+{
+	length++;
+	
+	str++;	
+	
+}
+   printf("The Length of string :%d\n",length);
+}
+
+int main()
+{
+	char name[20];
+	
+	printf("Enter a string : ");
+	gets(name);
+	
+	printf("\n");
+	
+	stringLength(name);
+	
+}
---
+++ Suraj/PR.8/pro.2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+// Develop program to find cubes of all elements from given 2D array using pointer with UDF
+
+void cubes()
+{
+	int a[5][5];
+	
+	int
+}
+
