06c602c44f292635efc5563c17b445f56df2e8c0
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-04-02 23:10:24 +0530

    Update pro.1.c

--- Suraj/PR.8/pro.1.c
+++ Suraj/PR.8/pro.1.c
@@ -2,30 +2,30 @@
 
 // develop a program that finds the length of a string using a pointer
 
-int stringLength(char *str)
+int stringLength(char *str)      // for create length of string
 {
 
 int length =0;
 
-while(*str!='\0')
+while(*str!='\0')               // for loop continues until string is null character
 {
-	length++;
+	length++;                  // for increment in length 
 	
-	str++;	
+	str++;	                   // for increment in pointer
 	
 }
-   printf("The Length of string :%d\n",length);
+   printf("The Length of string :%d\n",length);   // for input length of string
 }
 
 int main()
 {
 	char name[20];
 	
-	printf("Enter a string : ");
+	printf("Enter a string : ");       // for display string by user  
 	gets(name);
 	
 	printf("\n");
 	
-	stringLength(name);
+	stringLength(name);            // for function call at length of string
 	
 }
