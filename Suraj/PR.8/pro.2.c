73df24a0af6c4522d697cefb1dc140426ca2f059
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-04-02 23:04:27 +0530

    Update pro.2.c

--- Suraj/PR.8/pro.2.c
+++ Suraj/PR.8/pro.2.c
@@ -2,46 +2,42 @@
 
 // Develop program to find cubes of all elements from given 2D array using pointer with UDF
 
-void cubes(int *ptr, int rows, int cols)
+void cubesElements(int *ptr, int n)         // for pointer to the first element of 2D array and n for size of array
 {
- int i,j,n;
- 
- for(i=0; i<rows; i++)
- {
- 	for(j=0; j<cols; j++)
- 	{
- 	   n = *ptr;
- 	   
- 	   printf("\nCube of array [%d][%d]=[%d]\n",i,j,n*n*n);
- 	   
- 	   ptr++;
-	}
-	printf("\n");
- }
+    int i,j;
 
+    printf("\nCubes of All elements:\n\n");   // for input elements
+
+    for(i=0; i<n; i++)                        
+    {
+        for(j=0; j<n; j++)    
+        {
+            int num = *(ptr+i*n+j);         // for access element         
+            
+			printf("%4d ", num*num*num);    // for find cube of element and aligns output in array form
+        }
+        printf("\n");
+    }
 }
+
 void main()
 {
-	int rows,cols,i,j;
-	
-	printf("Enter Number of rows: ");
-	scanf("%d",&rows);
-	
-	printf("Enter Number of cols: ");
-	scanf("%d",&cols);
-	
-	int a[rows][cols];
-	
-	for(i=0; i<rows; i++)
-	{
-		for(j=0; j<cols; j++)
-		{
-			printf("a[%d][%d]",i,j);
-			scanf("%d",&a[i][j]);
-		}
-		
-	}
-	printf("\nCubes of All Elements -");
-	cubes(&a[0][0], rows, cols);
-}
+    int n,i,j;
 
+    printf("Enter array size: ");           // for input size of array
+    scanf("%d",&n);
+
+    int a[n][n];
+    
+    printf("\nEnter array Elements:\n");    // for input elements of array
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            printf("a[%d][%d] = ",i,j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    cubesElements(&a[0][0],n);                // for passes the address of first element and function call
+}
